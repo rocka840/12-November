84d2eacea75e7077f709252c113c5be2fac0d4f5
unknown <RocKa840@ltpemdomain.local>
2019-11-12 14:23:41 +0100

    jk

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ var myHTMLItem = document.getElementById("container");
 var myYellowBox = '<div class="box yellowBox"></div>';
 var myGreenBox = '<div class="box greenBox"></div>';
 
-myHTMLItem.innerHTML = myYellowBox + myGreenBox + myYellowBox + myGreenBox;
+var result = "";
+for (i = 0; i < 10; i++) {
+    result += myGreenBox + myYellowBox;
+    /*result = result + myYellowBox;
+      result = result + mxGreenBox;*/
+}
 
-for (;;)
+myHTMLItem.innerHTML = result;
+// myYellowBox + myGreenBox + myYellowBox + myGreenBox;
